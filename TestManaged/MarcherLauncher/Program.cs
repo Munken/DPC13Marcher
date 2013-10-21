c0f12fff62b28f4c572baf43aeadf5d14e8f894b
Munken <mm.munk@gmail.com>
2013-10-22 00:07:10 +0200

    First invocation

--- TestManaged/MarcherLauncher/Program.cs
+++ TestManaged/MarcherLauncher/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace MarcherLauncher
 {
     using ManagedCuda;
+    using ManagedCuda.VectorTypes;
     class Program
     {
         static void Main(string[] args)
@@ -17,6 +18,24 @@ namespace MarcherLauncher
             kernel.SetConstantVariable("d_edgeTable", Tables.EDGE_TABLE);
             kernel.SetConstantVariable("d_triTable", Tables.TRI_TABLE);
 
+            dim3 d = new dim3(10, 10, 10);
+            uint N = prod(d);
+            float iso = 0;
+            float3 min = new float3(0, 0, 0);
+            float3 stepsize = new float3(0.2f, 0.2f, 0.2f);
+            
+            CudaDeviceVariable<float3> result = new CudaDeviceVariable<float3>(N);
+            CudaDeviceVariable<uint> count = new CudaDeviceVariable<uint>(N);
+
+            kernel.Run(iso, d, min, stepsize, result, count);
+            
+
+            // (float isoValue, dim3 dims, float3 minX, float3 dx, float3* out, uint* count)
+        }
+
+        static uint prod(dim3 d)
+        {
+            return d.x * d.y * d.z;
         }
     }
 }
